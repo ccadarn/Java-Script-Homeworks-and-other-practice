7cfd6b9f571b0e33d62c5fbf4505bc735efdf5be
ccadarn <ccadarn@gmail.com>
2023-12-18 12:18:35 +0100

    Uploading my JS homeworks

---
+++ arraysHM.js
@@ -0,0 +1,144 @@
+/* Task: Find the Maximum Number
+
+Write a function that takes an array of numbers as an argument and returns the maximum number in the array.
+
+Create a function called findMaxNumber that accepts one parameter (an array).
+Inside the function, find and return the largest number in the array.
+Test your function with various arrays containing numbers and ensure it returns the correct maximum value.
+For instance:
+
+javascript
+Copy code
+findMaxNumber([3, 7, 2, 10, 5]); // Expected output: 10
+findMaxNumber([-1, -5, -2, -8]); // Expected output: -1
+findMaxNumber([17, 42, 5, 9, 21]); // Expected output: 42
+*/
+
+const findMaxNumber = (arr) => {
+    let highestNumber = arr[0]; // Set the initial highestNumber to the first element of the array
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > highestNumber) {
+            highestNumber = arr[i]; // Update highestNumber if a larger value is found
+        }
+    }
+
+    return highestNumber; // Return the highest number after iterating through the whole array
+};
+
+
+/* **NOTE**
+As it turned out all that can be achived with build in method 
+const maxNumber = Math.max(...numbers);
+also in the process learned about spread operator )
+*/
+
+
+console.log(findMaxNumber([3, 7, 2, 10, 5]));
+console.log(findMaxNumber([-5, -1, -2, -8]));
+console.log(findMaxNumber([17, 42, 5, 9, 21]));
+
+/*
+Task 2: Remove Duplicates
+
+Write a function that takes an array as an argument and returns a new array without any duplicate elements.
+
+Create a function called removeDuplicates that accepts one parameter (an array).
+Inside the function, create a new array that contains only the unique elements from the original array.
+Test your function with arrays containing duplicate elements and ensure it returns a new array with unique elements only.
+For example:
+
+javascript
+Copy code
+removeDuplicates([1, 2, 2, 3, 4, 4, 5]); // Expected output: [1, 2, 3, 4, 5]
+removeDuplicates(['apple', 'orange', 'apple', 'banana']); // Expected output: ['apple', 'orange', 'banana']
+Try implementing this function on your own to practice manipulating arrays.
+*/
+
+//----------------------------------------------------------------------
+
+//FAIL :(
+
+/* MY answer
+const removeDuplicates = (arr) => {
+    let noDupes = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!noDupes.includes(arr[i])) {
+            noDupes.push(arr[i]);
+        }
+    }
+    return noDupes;
+};
+
+*/ 
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//Correct answer
+const removeDuplicates = (arr) => {
+    let noDupes = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!noDupes.includes(arr[i])) {
+            noDupes.push(arr[i]);
+        }
+    }
+    return noDupes;
+};
+
+/* alternative using for loops
+const removeDuplicates = (arr) => {
+    let noDupes = [];
+    for (let i = 0; i < arr.length; i++) {
+        let isDuplicate = false;
+        for (let j = 0; j < noDupes.length; j++) {
+            if (arr[i] === noDupes[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            noDupes.push(arr[i]);
+        }
+    }
+    return noDupes;
+};
+*/
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Expected output: [1, 2, 3, 4, 5]
+console.log(removeDuplicates(['apple', 'orange', 'apple', 'banana'])); // Expected output: ['apple', 'orange', 'banana']
+
+
+//--------------------------------------------------
+/*Task: Reverse Array Elements
+
+Write a function that reverses the order of elements in an array without using the built-in reverse() method.
+
+Create a function called reverseArray that accepts one parameter (an array).
+Inside the function, reverse the order of elements in the array without using the reverse() method.
+Return the modified array with the reversed elements.
+For example:
+
+javascript
+reverseArray([1, 2, 3, 4, 5]); // Expected output: [5, 4, 3, 2, 1]
+reverseArray(['apple', 'orange', 'banana']); // Expected output: ['banana', 'orange', 'apple']
+Try implementing this function without using the reverse() method or any other built-in functions that directly reverse the array. You can use loops or any other logical approach to reverse the array elements.*/
+//----------------------------
+
+
+//Answer was CORRECT but I have doene it before in forLoopsHM so had some practice :)
+
+const reverseArray = (arr) => {
+    let newArr = []
+    for (let i = arr.length-1; i >= 0; i--) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(reverseArray(['apple', 'orange', 'banana']));
+
+/*There is a build in medhod - .reverse for that
+const array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output: [5, 4, 3, 2, 1]
+*/
---
+++ forLoopsHW.js
@@ -0,0 +1,240 @@
+//---------------------------------------------------------------------------------
+//Reverse Array: Write a function that takes an array as input and returns a new array with the elements in reverse order using a for loop.
+
+const reverseArr = (arr) => {
+    let newArr = [];
+    for (let i = arr.length-1; i >= 0; i--) {
+        newArr.push(arr[i])
+    }
+    return newArr;
+
+}
+
+
+console.log(reverseArr([1, 2, 3, 4, 'cat', 'dog', 16]))
+
+
+//-----------------------------------------------------------------------------------------------------------------------
+//Factorial Calculation: Create a function that calculates the factorial of a given number using a for loop. 
+//The factorial of a number 'n' is the product of all positive integers less than or equal to 'n'.
+
+const fac = (n) => {
+    sum = 1;
+    for (let i = 1; i <= n; i++) {
+       sum *= i
+    }
+    return sum
+}
+
+console.log(fac(3));
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+//Palindrome Check: Make a function that checks whether a given word is a palindrome (reads the same forward and backward). 
+//Use a for loop to compare characters from both ends of the word.
+
+const palindromeCheck = (word) => {
+    const arr = word.toLowerCase().split('');
+    const arr1 = [];
+    const arr2 = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        arr1.push(arr[i]);
+    }
+
+    for (let j = arr.length - 1; j >= 0; j--) {
+        arr2.push(arr[j]);
+    }
+
+    if (arr1.join('') === arr2.join('')) {
+        return `${word} is a palindrome`;
+    } else {
+        return `${word} isn't a palindrome, try something else`;
+    }
+};
+
+console.log(palindromeCheck('fire'));
+console.log(palindromeCheck('Madam'));
+console.log(palindromeCheck('madam'));
+console.log(palindromeCheck('12321'));
+console.log(palindromeCheck('Able was I ere I saw Elba'));
+
+//--------------------------------------------------------------------------------------------
+//Fibonacci Sequence: Write a function to generate the Fibonacci sequence up to a specified number of terms. 
+//The Fibonacci sequence starts with 0 and 1, and each subsequent term is the sum of the two preceding ones.
+
+
+
+//------------------------------------------------------
+//Prime Number Check: Create a function that checks if a given number is a prime number (a number greater than 1 that has no divisors other than 1 and itself). 
+//Use a for loop to test the divisibility of the number.
+
+const prime = (num) => {
+        
+    if (num < 1) {return `${num} is NOT a PRIME number because it is SMALLER than 1`}
+
+    for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                return `${num} is NOT a PRIME number`
+            }
+        } 
+    return `${num} is a PRIME number`
+} 
+
+
+
+
+console.log(prime(4));
+console.log(prime(7));
+console.log(prime(1));
+
+
+//=-------------------------
+const timer = (arr) => {
+    if (arr.length < 10 || arr.indexOf(0) === -1) {
+        console.log('Array should be more than 0 and at least 10 (0-10), and must contain 0');
+        return; // Exit the function
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === 10) {
+            console.log('10 seconds until launch!');
+        } else if (arr[i] === 5) {
+            console.log('5 seconds until launch!');
+        } else if (arr[i] === 3) {
+            console.log('3');
+        } else if (arr[i] === 2) {
+            console.log('2');
+        } else if (arr[i] === 1) {
+            console.log('1');
+        } else if (arr[i] === 0) {
+            console.log('Launch!');
+        }
+    }
+};
+
+timer([1, 2, 3, 4, 5, 6, 7, 8]); // Logs the error message to the console
+timer([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); // Executes the timer function
+
+
+/*Create a function called countVowels that takes a string as an argument and counts the number of vowels (a, e, i, o, u - both lowercase and uppercase)
+ in the given string using a for loop. The function should return the total count of vowels found in the string.
+Feel free to give it a try and experiment with the for loop to iterate through the characters of the string and count the vowels! */
+
+const countVowels = (string) => {
+ let cleanString = string.toLowerCase().split('');
+ let sum = 0
+ for (let i = 0; i <= cleanString.length; i++) {
+    if (cleanString[i]==='a') {
+        sum++;
+    } else if (cleanString[i]==='e') {
+        sum++;
+    } else if (cleanString[i]==='i') {
+        sum++;
+    } else if (cleanString[i]==='o') {
+        sum++;
+    } else if (cleanString[i]==='u') {
+        sum++;
+    }
+}
+  if (sum === 0) {
+    return `There are no vowels in '${string}'`;
+  } else
+  return `There are a total of ${sum} vovels in '${string}'`;
+}
+
+
+ console.log(countVowels("The quick brown fox jumps over the lazy dog."));
+ console.log(countVowels("Fly thy cryptic glyphs."));
+ console.log(countVowels("Gymnymphs fly crypts."));
+ console.log(countVowels("Hello! How are you?"));
+
+
+
+ // alternative
+ const countVowels2 = (string) => {
+    let cleanString = string.toLowerCase().split('');
+    let sum = 0;
+
+    for (let i = 0; i < cleanString.length; i++) {
+        if (cleanString[i] === 'a' || cleanString[i] === 'e' || cleanString[i] === 'i' || cleanString[i] === 'o' || cleanString[i] === 'u') {
+            sum++;
+        }
+    }
+
+    if (sum === 0) {
+        return `There are no vowels in '${string}'`;
+    } else {
+        return `There are a total of ${sum} vowels in '${string}'`;
+    }
+};
+
+console.log(countVowels2("The quick brown fox jumps over the lazy dog."));
+console.log(countVowels2("Fly thy cryptic glyphs."));
+console.log(countVowels2("Gymnymphs fly crypts."));
+console.log(countVowels("Hello! How are you?"));
+
+// alternative 2 ))
+const countVowels3 = (string) => {
+    let sum = 0;
+
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i].toLowerCase();
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+            sum++;
+        }
+    }
+
+    if (sum === 0) {
+        return `There are no vowels in '${string}'`;
+    } else {
+        return `There are a total of ${sum} vowels in '${string}'`;
+    }
+};
+
+console.log(countVowels3("The quick brown fox jumps over the lazy dog."));
+console.log(countVowels3("Fly thy cryptic glyphs."));
+console.log(countVowels3("Gymnymphs fly crypts."));
+
+/* Write a JavaScript program that takes a number n (where n is a positive integer) as input and generates a multiplication table from 1 to n using nested loops. 
+For example, if the user inputs 5, the output should be:
+
+Copy code
+1  2  3  4  5
+2  4  6  8 10
+3  6  9 12 15
+4  8 12 16 20
+5 10 15 20 25
+Your task is to create this multiplication table using nested loops in JavaScript. Ensure that the output is properly formatted.
+
+Remember to validate the input to make sure it's a positive integer greater than 0 before generating the multiplication table.
+
+Feel free to use any approach or method in JavaScript that you find suitable. Happy coding! */
+
+const mtable = (n) => {
+    if (typeof n !== 'number' || n < 0) {
+        return `${n} should be a positive integer`;
+    }
+    
+    const maxDigits = (n * n).toString().length; // Determine the maximum digits in the table
+    
+    for (let i = 1; i <= n; i++) {
+        let row = '';
+        for (let j = 1; j <= n; j++) {
+            let product = i * j;
+            let spacesNeeded = maxDigits - product.toString().length; // Calculate spaces needed
+            
+            // Add spaces for alignment based on the number of digits
+            while (spacesNeeded > 0) {
+                row += ' ';
+                spacesNeeded--;
+            }
+            
+            row += product + ' ';
+        }
+        console.log(row);
+    }
+};
+
+console.log(mtable(5));
+console.log(mtable(1));
