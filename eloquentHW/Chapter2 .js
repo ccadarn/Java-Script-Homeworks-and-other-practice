c53a89bd088254a72e9b08f729765aeb097cb9d2
ccadarn <ccadarn@gmail.com>
2024-01-23 17:25:04 +0100

    modified:   eloquentHW/Chapter2 .js - updated structure modified:   eloquentHW/Chapter3.js - Finished and updated Chapter3 excercises

--- eloquentHW/Chapter2 .js
+++ eloquentHW/Chapter2 .js
@@ -1,7 +1,27 @@
 //Eloquent JS Home work
 
 //----------------------------------------------------------------
-//Task 1
+//Excercise 1
+/*
+Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+
+*/
+
+//------------------------------------------------------------------------
 
 // 1 try
 /*
@@ -32,7 +52,20 @@ for(let i=0; i <=7; i++){
 
 //---------------------------------------------
 
-//Task2 - FizzBuzz
+//Excercise 2
+
+/*
+FizzBuzz
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+(This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+
+*/
+
+//----------------------------------------------
+
 /*
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
@@ -50,6 +83,27 @@ for (let i = 1; i <= 100; i++) {
 //-----------------------------------------
 
 //Task 3 - Chess board
+
+/*
+Chessboard
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+
+/*
+
+//-------------------------------------------------
+
 /*let size = 8;
 let grid = '';
 
@@ -66,15 +120,21 @@ for (let i = 1; i <= size * size; i++) {
 console.log(grid);
 /*
 
+//------------------------------------------
+
+
 /*
 Results:
-1st task made several minor mistakes
-2nd task was ruined by bild in AI autocompletion in Visual studio - it just provided a solution at the very start (
-3rd task was solved but I am lasking inner loop practice and understanding, was on the right track but was not able to solve it in the end (
+1st excercise: made several minor mistakes
+2nd excercise: was ruined by build in AI autocompletion plugin in Visual studio - it just provided a solution at the very start (turned it off)
+3rd excercise: was solved but I am lacking inner loop practice and understanding, was on the right track but was not able to solve it in the end (
 
-But it was refreshing )
+But it was interesting )
 will continue...
 */
+
+//--- Below is just my personal excercise, was trying to build a multiplication table...
+
 
 for (let i = 1; i <= 10; i++) {
     let row = '';
--- eloquentHW/Chapter3.js
+++ eloquentHW/Chapter3.js
@@ -1,10 +1,24 @@
 
 
 //Excercise 1
-//Write a 'min' function to find a min bumber out of tow parameters
+/*
+Minimum
+The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+
+// Your code here.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+*/
+
+//----------------------------------------------------------------
 
 /*
-Try1
+//Try1
+
 let min = (a, b) => {
     if (a < b){
         return a;
@@ -22,7 +36,8 @@ console.log(min(0, 0));
 */
 
 /*
-Try 2 (want to shorten it)
+//Try 2 (want to shorten it)
+
 let min = (a, b) => a < b ? a : (a > b ? b : `Both numbers are equal: ${a}`)
 
 console.log(min(0, 10));
@@ -30,12 +45,36 @@ console.log(min(0, -10));
 console.log(min(0, 0));
 */
 
+//---------------------------------------------
 
 //Excercise 2
-// *Zero is even
-// *One is odd
-// use N-2 to find if number (N) is even or odd, using recursion function
 
+/*
+Recursion
+We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+
+Zero is even.
+
+One is odd.
+
+For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+// Your code here.
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+*/
+
+/*
+//Normal solution
 
 let isEven = (number) => {
     let even = 0;
@@ -61,6 +100,8 @@ console.log(isEven(50)); // Output: true
 console.log(isEven(75)); // Output: false
 console.log(isEven(-1)); // `Input number (-1) should be bigger than 0`
 
+*/
+
 //Trying loops
 
 /*
@@ -84,4 +125,92 @@ console.log(isEven(50)); // Output: true
 console.log(isEven(75)); // Output: false
 console.log(isEven(-1)); // `Input number (-1) should be bigger than 0`
 
+*/
+
+//----------------------------------------------------------------------------
+//Exercise 3
+
+/*
+Bean counting
+You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
+*/
+
+//---First Part (countBs)
+
+/*
+let countBs = (string) => {
+    let numberOfB = 0;
+    for(let i=0; i<=string.length-1; i++) {
+        if (string[i] === 'B') {
+            numberOfB ++;
+        }
+    }
+    return numberOfB;
+}
+
+console.log(countBs("BBC")); //2
+*/
+
+//---Second part (countChar)
+
+/*
+let countChar = (string, letter) => {
+    let numberOfChar = 0;
+    for(let i=0; i<=string.length-1; i++) {
+        if (string[i] === letter) {
+            numberOfChar ++;
+        }
+    }
+    return numberOfChar;
+}
+
+
+console.log(countChar("kakkerlak", "k")) //4
+console.log(countChar("bumble bee", "b")) //3
+console.log(countChar("Bumble Bee", "B")) //2
+*/
+
+//---Using 'for...of' loop (cleaner method)
+
+let countChar = (string, letter) => {
+    let numberOfChar = 0;
+    for(let char of string) {
+        char === letter && numberOfChar++;
+    }
+    return numberOfChar;
+}
+
+
+console.log(countChar("kakkerlak", "k")) //4
+console.log(countChar("bumble bee", "b")) //3
+console.log(countChar("Bumble Bee", "B")) //2
+
+/*
+
+//-------------------------------
+
+Results:
+1st excercise: at this stage didnt have any touble which is very good )
+2nd excercise: still need to practice recursion functions, I generaly understand it but practice is needed 100%
+3rd excercise: no truble at this stage I had a lot of practice with for loop, so simple for loop iteration wasnt a problem. 
+Need more practice with for...of and ternary operator, when the condition is true and do nothing when it's false
+
+condition && expressionIfTrue; - i like using ternary operator a lot when if...else arises so this code was new to me (so far havent came across this)
+
+---Practice:---
+1) Recursion functions;
+2) for...of loops
+3) Ternary operator when condition && expressionIfTrue;
+
+Overal happy with the results ^^
 */
