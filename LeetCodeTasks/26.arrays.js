a7e21d9721b98bcfe0c651a54b6fee66f7befbd1
ccadarn <139007118+ccadarn@users.noreply.github.com>
2025-02-10 18:33:34 +0100

    New excercise version solution added

--- LeetCodeTasks/26.arrays.js
+++ LeetCodeTasks/26.arrays.js
@@ -89,3 +89,25 @@ var removeDuplicates = function(nums) {
 
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); // → [0, 1, 2, 3, 4]
 console.log(removeDuplicates([-5,0,1,1,1,2,2,3,3,4])); // → [-5, 0, 1, 2, 3, 4]
+
+
+// new test version for this excercise
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0; 
+
+    let uniqueIndex = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[uniqueIndex]) { 
+            uniqueIndex++; 
+            nums[uniqueIndex] = nums[i]; 
+        }
+    }
+
+    return uniqueIndex + 1; 
+};
+
+// Test cases
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4,4,4,5]));  // [0,1,2,3,4,5]
+console.log(removeDuplicates([-5,0,1,1,1,2,2,3,3,4,4,4,5])); // [-5,0,1,2,3,4,5]
+console.log(removeDuplicates([3,4,4,4,5])); // [3,4,5]
